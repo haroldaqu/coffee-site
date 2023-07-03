0298ef6d38b8abc69b35a6888cee7da64643e21e
haroldaqu <103527480+haroldaqu@users.noreply.github.com>
2023-07-03 11:39:02 -0700

    Finish home page

--- src/components/home/Newsletter.jsx
+++ src/components/home/Newsletter.jsx
@@ -2,12 +2,13 @@ const Newsletter = () => {
     return (
         <div className="home_newsletter">
             <div className="">
-        <h1>Stay updated to get our special offers</h1>
-                                <img src="/images/vectors/line-scratch.png" alt="" className="vector"/>
-        <div className="newsletter_form">
-            <input type="text"  placeholder="EMAIL ADDRESS"/>
-            <button children={"SUBSCRIBE"} ></button>
-        </div>
+                <h1>Stay updated to get our special offers</h1>
+                <img src="/images/vectors/line-scratch.png" alt="" className="vector"/>
+                <img src="/images/vectors/microphone.png" alt="" className="vector vector--microphone" />
+                <div className="newsletter_form">
+                    <input type="text"  placeholder="EMAIL ADDRESS"/>
+                    <button children={"SUBSCRIBE"} ></button>
+                </div>
             </div>
         </div>
     )
