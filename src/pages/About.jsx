c600ed3c41eec0d69e08e5518233c3e015a7b36e
haroldaqu <103527480+haroldaqu@users.noreply.github.com>
2023-07-03 13:52:26 -0700

    finish about header

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,19 +5,25 @@ import Button from "../layouts/Button"
 const About  = () => {
     return(
         <main className="about">
-            
+
             <section className="about_header">
-                <h1>Fuel your day with our exceptional coffee creations</h1>
+                <h1>
+                    Fuel your day with our <br></br> exceptional coffee creations
+                    <img src="/images/vectors/star.png" alt="" className="vector vector--star"/>
+                    <img src="/images/vectors/line-scratch.png" alt="" className="vector vector--line"/>
+                </h1>
                 <div className="">
-                    <img src="" alt="" />
-                    <img src="" alt="" />
+                    <img src="/images/about/header_one.png" alt="" />
+                    <img src="/images/about/header_two.png" alt="" />
                     <div className="">
-                        <img src="" alt="" />
+                        <img src="/images/about/header_three.png" alt="" />
                         <p>We have a possion for coffee and people. Make sure you always have your favorite cup of coffee around to start your day off right. We have a possion for coffee and people. Make sure you always have your favorite cup of coffee around to start your day off right.  e. Make sure you always have your favorite cup of coffee around to start your day off right. </p>
                         <Button children={"View Products"} />
                     </div>
                 </div>
             </section>
+
+
 
         </main>
     )
